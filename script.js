ec0baeb541aa02263fe71ccc67b8e88e54cc0c54
Ben Reid <beared1194@gmail.com>
2019-04-15 00:48:06 +0100

    Updated with use of Modals

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 } 
+
+var modal = document.getElementById('myModal');
+var link = document.getElementsByClassName("open-modal");
+var modalImage = document.getElementById('modal-img');
+var cardName = document.getElementById('modalCardName');
+var span = document.getElementsByClassName("close")[0];
+
+for (i = 0; i < link.length; i++) {
+    link[i].onclick = function () {
+      var newName = this.innerHTML;
+      cardName.innerHTML = newName;
+      modalImage.src = '../source_images/' + newName + '.jpg';
+      modal.style.display = "block";
+    }
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modalImage.src = "./card-back-default.jpg"
+        modal.style.display = "none";
+    }
+}
